6f7905095eafa60ef50473663ec48d4e60279821
Jaakko Manitla <jaakko.mantila@fisplay.com>
2022-09-25 13:14:52 +0300

    changed debug variable name

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ export MQTT_USER="$(jq --raw-output '.mqtt_user' $CONFIG_PATH)"
 export MQTT_PASS="$(jq --raw-output '.mqtt_pass' $CONFIG_PATH)"
 export MQTT_CLIENT_ID="$(jq --raw-output '.mqtt_client_id' $CONFIG_PATH)"
 export MQTT_DISCOVERY_PREFIX="$(jq --raw-output '.mqtt_discovery_prefix' $CONFIG_PATH)"
-export DEBUG="$(jq --raw-output '.debug' $CONFIG_PATH)"
+export DEBUG="$(jq --raw-output '.server_debug' $CONFIG_PATH)"
 
 echo "Starting local server to listen growatt dataloggers on port 5279"
 
